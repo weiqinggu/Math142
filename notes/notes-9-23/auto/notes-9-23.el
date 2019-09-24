8ded1500e8bc8d5b59f382026cb0ba87f20445c1
Joseph Gardi <joeyg@ubnet.io>
2019-09-23 21:53:22 -0700

    added the notes

---
+++ notes/notes-9-23/auto/notes-9-23.el
@@ -0,0 +1,11 @@
+(TeX-add-style-hook
+ "notes-9-23"
+ (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-class-options
+                     '(("article" "12pt")))
+   (TeX-run-style-hooks
+    "latex2e"
+    "article"
+    "art12"))
+ :latex)
+
