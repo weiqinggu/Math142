d1b1407100d82ed19d27501ba3965c463aebbf2e
Joseph Gardi <joeyg@ubnet.io>
2019-11-18 00:18:35 -0800

    added hw8 solutions

---
+++ notes/notes-11-11/auto/main.el
@@ -0,0 +1,22 @@
+(TeX-add-style-hook
+ "main"
+ (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-class-options
+                     '(("hmcpset" "12pt" "letterpaper")))
+   (TeX-add-to-alist 'LaTeX-provided-package-options
+                     '(("geometry" "margin=1in")))
+   (TeX-run-style-hooks
+    "latex2e"
+    "macros"
+    "hmcpset"
+    "hmcpset12"
+    "geometry"
+    "graphicx"
+    "amsthm"
+    "enumitem"
+    "amsmath"
+    "cancel")
+   (TeX-add-symbols
+    "Tr"))
+ :latex)
+
